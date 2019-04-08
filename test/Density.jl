48330609943c1d54500d2c57155595a8d4f1d86e
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-08 04:16:36 +0000

    Snapshot of branch master taken on 2019-04-08T00:16:35.833-04:00 from "https://github.com/UnofficialJuliaMirror/VoronoiCells.jl-e3e34ffb-84e9-5012-9490-92c94d0c60a4"

---
+++ test/Density.jl
@@ -0,0 +1,11 @@
+using VoronoiCells
+using Base.Test
+
+@testset "Density" begin
+	# Corners of the unit square
+	x = [1.0 ; 0.0 ; 0.0 ; 1.0]
+	y = [1.0 ; 1.0 ; 0.0 ; 0.0]
+
+	@test density(x, y) ≈ sqrt(2)/2
+end
+
